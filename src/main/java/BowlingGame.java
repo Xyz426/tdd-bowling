08634e7ae2706cfa43721c3b65408a510a095e56
xyz <xyzzhybz@163.com>
2020-08-03 18:48:32 +0800

    finished when any hit less than 10

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,11 +1,20 @@
 public class BowlingGame {
 
-    public int calculate(int[] arr_balls) throws RuntimeException {
+    public int calculateWhenExc(int[] arr_balls) throws RuntimeException {
         for (int i = 0; i < arr_balls.length; i++) {
             if (arr_balls[i] < 0 || arr_balls[i] > 10) {
                 throw new RuntimeException("input balls is error");
             }
         }
         return 0;
+    }
+
+    public int calculateHitLessThan10(int[] arr_balls){
+        int totalScore = 0;
+        for (int i = 0; i <arr_balls.length; i++) {
+            totalScore += arr_balls[i];
+        }
+
+        return totalScore;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -13,8 +13,19 @@ public class BowlingGameTest {
         int arr_balls[] = {1,2,3,4,5,-1,6,3,21,1};
 
         assertThrows(RuntimeException.class,()->{
-            bowlingGame.calculate(arr_balls);
+            bowlingGame.calculateWhenExc(arr_balls);
         });
     }
 
+    @Test
+    void should_get_score_when_any_hit_less_than_10(){
+        //Given
+        int arr_balls[] = {0,1,2,3,4,5,6,7,8,9};
+
+        //when
+        int totalScore = bowlingGame.calculateHitLessThan10(arr_balls);
+
+        //then
+        assertEquals(45,totalScore);
+    }
 }
